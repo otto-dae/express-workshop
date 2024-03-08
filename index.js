aa859b7fb0000438f9425880b1caaf8f9d47182f
Otto <otto.acha.28@gmail.com>
2024-03-08 15:43:22 -0600

    Poekdex implement

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const pokedex = require('./pokedex.json')
 
 /* 
 Verbos HTTP 
@@ -13,13 +14,18 @@ Verbos HTTP
 
 app.get("/",(req, res, next) =>
 {
-    res.status(200)
-    res.send("Hello world");
+    const pokemon = pokedex.pokemon;
+    res.send(pokemon);
 });
 
+app.get("/:name",(req, res, next) =>
+{
+    console.log(req.params.name);
+    res.status(200);
+    res.send("Hola " + req.params.name);
+});
 
-
-app.listen(3000, () =>
+app.listen(process.env.PORT || 3000, () =>
 {
     console.log("Server is Running...");
 });
