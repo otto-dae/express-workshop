0a4eaf5f8aeaa41667f613d682e2596880e74498
Otto <otto.acha.28@gmail.com>
2024-03-15 13:50:36 -0600

    ada

--- index.js
+++ index.js
@@ -11,14 +11,13 @@ const {pokemon} = require('./pokedex.json')
 */
 app.get("/",(req, res, next) =>
 {
-    res.status(200);
-    res.send("Bienvenido aL pokedex");
+    return res.status(200).send("Bienvenido aL pokedex");
 });
 
 app.get("/pokemon",(req, res, next) =>
 {
-    res.status(200);
-    res.send(pokemon); 
+    return res.status(200).send(pokemon); 
+    
 });
 
 app.get('/pokemon/:id([0-9]{1,3})', (req, res, next)  =>
@@ -26,15 +25,32 @@ app.get('/pokemon/:id([0-9]{1,3})', (req, res, next)  =>
     const id = req.params.id -1;
     if(id >= 0 && id <= 150 )
     {
-        res.status(200);
-        res.send(pokemon[req.params.id - 1]);
+        return res.status(200).send(pokemon[req.params.id - 1]);
+        
     }
-    else
-    {
-        res.status(404);
-        res.send("Pokemon not found");
-    }
+        return res.status(404).send("Pokemon not found"); 
+});
+
+app.get('/pokemon/:name[A-Za-z]+)', (req, res, next) =>
+{
+    const name = req.params.name;;
     
+    /*for (i = 0; i < pokemon.length; i++)
+    {
+        if(pokemon[i].name.toUpperCase() == name.toUpperCase())
+        {
+            return res.status(200).send(pokemon[i]);
+        }
+    }*/
+
+    const pk = pokemon.filter((p) => 
+    {
+        if(p.name.toUpperCase() == name.toUpperCase())
+        {
+            return p;
+        }
+    });
+    return res.status(200).send(pk);
 });
 
 app.listen(process.env.PORT || 3000, () =>
