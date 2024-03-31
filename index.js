6e2b72039650437f1d7bc8b84321c51e3e3f4aef
Otto <otto.acha.28@gmail.com>
2024-03-31 10:38:15 -0600

    clean

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
 const express = require('express');
 const app = express();
 const {pokemon} = require('./pokedex.json')
-
-/*
-    GET -Obtener recursos
-    POST - Almacenar/crear Recursos
-    PATCH - Modificar una parte de un recurso
-    PUT - Modificar un recurso
-    DELETE - Borrar un recurso
-*/
 app.get("/",(req, res, next) =>
 {
     return res.status(200).send("Bienvenido aL pokedex");
 });
-
 app.get("/pokemon",(req, res, next) =>
 {
     return res.status(200).send(pokemon); 
     
 });
-
 app.get('/pokemon/:id([0-9]{1,3})', (req, res, next)  =>
 {
     const id = req.params.id -1;
@@ -30,7 +20,6 @@ app.get('/pokemon/:id([0-9]{1,3})', (req, res, next)  =>
     }
         return res.status(404).send("Pokemon not found"); 
 });
-
 app.get('/pokemon/:name([A-Za-z]+)', (req, res, next) =>
 {
     const name = req.params.name;
@@ -45,10 +34,8 @@ app.get('/pokemon/:name([A-Za-z]+)', (req, res, next) =>
     {
         return res.status(200).send(pk);
     }
-
     return res.status(404).send("Pokemon not found")
 });
-
 app.listen(process.env.PORT || 3000, () =>
 {
     console.log("Server is Running...");
