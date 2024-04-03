aa714746ced9baec1bb812a14c72e69e12155a50
Otto <otto.acha.28@gmail.com>
2024-04-03 12:25:02 -0600

    sql

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,16 @@
 const express = require("express")
 const pokemon = express.Router();
-const pk = require('../pokedex.json').pokemon;
+const db = require("../config/database")
 
 pokemon.post("/",(req, res, next) =>
 {
     return res.status(200).send(req.body.name); 
     
 });
-pokemon.get("/",(req, res, next) =>
+pokemon.get("/", async (req, res, next) =>
 {
-    return res.status(200).send(pk); 
-    
+    const pkmn =  await db.query("SELECT * FROM pokemon")
+    return res.status(200).json(pkmn);     
 });
 pokemon.get('/:id([0-9]{1,3})', (req, res, next)  =>
 {
